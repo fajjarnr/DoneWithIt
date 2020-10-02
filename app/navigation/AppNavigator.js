057b7fe520d6b75a0d62144a53c3bd11564924da
fajjarnr <zfajart@gmail.com>
2020-10-02 15:53:17 +0700

    adding icon on bottom bar

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,18 +1,36 @@
 import React from "react";
 
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 
 import AccountScreen from "../screens/AccountScreen";
 import ListingEditScreen from "../screens/ListingEditScreen";
 import FeedNavigator from "./FeedNavigator";
+import { color } from "react-native-reanimated";
 
 const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => (
   <Tab.Navigator>
-    <Tab.Screen name="Listings" component={FeedNavigator} />
+    <Tab.Screen
+      name="Listings"
+      component={FeedNavigator}
+      options={{
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="home" color={color} size={size} />
+        ),
+      }}
+    />
     <Tab.Screen name="ListingEditScreen" component={ListingEditScreen} />
-    <Tab.Screen name="Account" component={AccountScreen} />
+    <Tab.Screen
+      name="Account"
+      component={AccountScreen}
+      options={{
+        tabBarIcon: ({ color, size }) => (
+          <MaterialCommunityIcons name="account" color={color} size={size} />
+        ),
+      }}
+    />
   </Tab.Navigator>
 );
 
