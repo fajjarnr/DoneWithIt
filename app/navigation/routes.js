c06069edd2f23e28c420fe10bc371a7a9f4ee604
fajjarnr <zfajart@gmail.com>
2020-10-02 15:42:59 +0700

    adding routes

---
+++ app/navigation/routes.js
@@ -0,0 +1,9 @@
+import { object } from "yup";
+
+export default object.freeze({
+  LISTING_DETAILS: "ListingDetails",
+  LISTING_EDIT: "ListingEdit",
+  LOGIN: "Login",
+  MESSAGES: "Messages",
+  REGISTER: "Register",
+});
